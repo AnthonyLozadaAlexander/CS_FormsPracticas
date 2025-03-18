c57291e3b66115a5b0cf00b0062360944973639c
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-17 19:38:00 -0500

    add Forms_CalculoDeSalario project to solution and refactor variable names in Form1

--- Forms_Calculadora/Form1.cs
+++ Forms_Calculadora/Form1.cs
@@ -22,7 +22,7 @@ public partial class Form1 : Form
     {
         decimal v1 = this.nud_Op1.Value;
         decimal v2 = this.nud_Op2.Value;
-
+        
         decimal total = v1 + v2;
 
         this.txt_Resultado.Text = total + "";
@@ -30,21 +30,21 @@ public partial class Form1 : Form
 
     private void btn_resta_Click(object sender, EventArgs e)
     {
-        decimal v1 = this.nud_Op1.Value;
-        decimal v2 = this.nud_Op2.Value;
+        decimal Valor1 = this.nud_Op1.Value;
+        decimal Valor2 = this.nud_Op2.Value;
 
-        if (v1 < v2)
+        if (Valor1 < Valor2)
         {
             MessageBox.Show("El primer valor debe ser mayor al segundo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             return;
         }
-        else if (v2 == 0)
+        else if (Valor2 == 0)
         {
-            this.txt_Resultado.Text = v1 + "";
+            this.txt_Resultado.Text = Valor1 + "";
         }
         else
         {
-            decimal total = v1 - v2;
+            decimal total = Valor1 - Valor2;
             this.txt_Resultado.Text = total + "";
         }
     }
@@ -74,6 +74,7 @@ public partial class Form1 : Form
             this.txt_Resultado.Text = total + "";
         }
     }
+    
     private void txt_Resultado_TextChanged(object sender, EventArgs e)
     {
         
