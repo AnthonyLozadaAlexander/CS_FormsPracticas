16783faa2a96a719bf714f90236aacf28e26acd9
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-17 22:23:27 -0500

    refactor btnLimpiar and btnCalcular methods for improved functionality and error handling

--- Forms_CalculoDeSalario/Form1.cs
+++ Forms_CalculoDeSalario/Form1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices.JavaScript;
+
 namespace Forms_CalculoDeSalario;
 
 public partial class Form1 : Form
@@ -48,23 +50,54 @@ public partial class Form1 : Form
     
     private void btnLimpiar_Click(object sender, EventArgs e)
     {
-        txtNombre.Clear();
+        /*txtNombre.Clear();
         txtImpuesto15.Clear();
         txtHorasExtras.Clear();
         txtSalarioBase.Clear();
         txtSalarioNeto.Clear();
         txtHorasTrabajadas.Clear();
-        txtSalarioxHora.Clear();
+        txtSalarioxHora.Clear();*/
+        
+        foreach(Control datos in this.groupBox1.Controls) // por cada control en el groupBox1.Controls
+        {
+            if(datos is TextBox) // si datos es un TextBox
+            {
+                datos.Text = string.Empty; // limpiara cada textBox del group1
+            }
+        }
+
+        foreach (Control datosR in this.groupBox2.Controls) // por cada control en el groupBox2.Controls
+        {
+            if (datosR is TextBox) // si datos es un TextBox
+            {
+                datosR.Text = ""; // limpiara cada textBox del group2
+            }
+        }
     }
 
     private void btnCalcular_Click(object sender, EventArgs e)
-    {
-        String nombre = txtNombre.Text;
-        float SalarioxHora = float.Parse(txtSalarioxHora.Text);
-        float SalarioBase = float.Parse(txtSalarioBase.Text);
-        float SalarioNeto = float.Parse(txtSalarioNeto.Text);
-        float HorasExtra = float.Parse(txtHorasExtras.Text);
-        float HorasTrabajadas = float.Parse(txtHorasTrabajadas.Text);
-        float Impuesto15 = float.Parse(txtImpuesto15.Text);
+    { 
+        float SalarioxHora, SalarioBase, SalarioNeto, HorasExtra, HorasTrabajadas, Impuesto15;
+        try
+        { 
+            
+         SalarioxHora = float.Parse(txtSalarioxHora.Text);
+         HorasExtra = float.Parse(txtHorasExtras.Text);
+         HorasTrabajadas = float.Parse(txtHorasTrabajadas.Text);
+
+         SalarioBase = (SalarioxHora * HorasTrabajadas) + (2f * (SalarioxHora * HorasExtra));
+         Impuesto15 = (SalarioBase * 0.15f);
+         SalarioNeto = SalarioBase - Impuesto15;
+
+         txtSalarioBase.Text = SalarioBase.ToString();
+         txtImpuesto15.Text = Impuesto15.ToString();
+         txtSalarioNeto.Text = SalarioNeto.ToString();
+         
+        }
+        catch (Exception error) // error es el nombre de la variable que se le asigna al catch (Exception "error")
+        {
+            MessageBox.Show("Error:\n" + error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        
     }
 }
